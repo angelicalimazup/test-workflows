270d03d1da4b577d000be44f2edce23e14892828
Angelica Lima <65919486+angelicalimazup@users.noreply.github.com>
2021-07-28 22:25:11 +0100

    test

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ HASH_COMMIT = os.environ['SOURCE_COMMIT']
 
 def workflow_status(workflowFile):
     #endpoint = "https://api.github.com/repos/angelicalimazup/testPipes/actions/workflows/"+ workflowFile + "/runs"
-    endpoint = "https://api.github.com/repos/angelicalimazup/testPipes/commits/"+HASH_COMMIT+"/statuses"
+    endpoint = "https://api.github.com/repos/angelicalimazup/testPipes/statuses/"+HASH_COMMIT
     headers = {"accept": "application/vnd.github.v3+json",
                "content-type": "application/json",
                "authorization": "Bearer " + GITHUB_TOKEN}
@@ -16,10 +16,10 @@ def workflow_status(workflowFile):
 
 
 if __name__ == '__main__':
-    time.sleep(60)
+    #time.sleep(60)
     workflowFile = "failWorkflow.yml"
     # while workflow_status(workflowFile) == 'queued':
     #     print(workflow_status(workflowFile))
     #     time.sleep(2)
-
+print(HASH_COMMIT)
 print(workflow_status(workflowFile))
