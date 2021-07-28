c55dd95e77ae9b77548e0d1540aed1b712ee3cba
Angelica Lima <65919486+angelicalimazup@users.noreply.github.com>
2021-07-28 22:01:25 +0100

    test

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ GITHUB_TOKEN = os.environ['TOKEN_GITHUB']
 HASH_COMMIT = os.environ['SOURCE_COMMIT']
 
 def workflow_status(workflowFile):
-    endpoint = "https://api.github.com/repos/angelicalimazup/testPipes/actions/workflows/"+ workflowFile + "/runs"
-    #endpoint = "https://api.github.com/repos/angelicalimazup/testPipes/commits/"+HASH_COMMIT+"/statuses"
+    #endpoint = "https://api.github.com/repos/angelicalimazup/testPipes/actions/workflows/"+ workflowFile + "/runs"
+    endpoint = "https://api.github.com/repos/angelicalimazup/testPipes/commits/"+HASH_COMMIT+"/statuses"
     headers = {"accept": "application/vnd.github.v3+json",
                "content-type": "application/json",
                "authorization": "Bearer " + GITHUB_TOKEN}
