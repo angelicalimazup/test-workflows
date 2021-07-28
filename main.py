d0736930935dbeb76d0d28cfe19b2a97b8b300a9
Angelica Lima <65919486+angelicalimazup@users.noreply.github.com>
2021-07-28 20:15:25 +0100

    test

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import requests
 import time
 
 GITHUB_TOKEN = os.environ['TOKEN_GITHUB']
-
+HASH_COMMIT = os.environ['SOURCE_COMMIT']
 
 def workflow_status(workflowFile):
-    endpoint = "https://api.github.com/repos/angelicalimazup/testPipes/actions/workflows/" + workflowFile + "/runs"
+    #endpoint = "https://api.github.com/repos/angelicalimazup/testPipes/actions/workflows/"+ workflowFile + "/runs"
+    endpoint = "https://api.github.com/repos/angelicalimazup/testPipes/commits/"+HASH_COMMIT+"/status"
     headers = {"accept": "application/vnd.github.v3+json",
                "content-type": "application/json",
                "authorization": "Bearer " + GITHUB_TOKEN}
@@ -16,8 +17,8 @@ def workflow_status(workflowFile):
 
 if __name__ == '__main__':
     workflowFile = "failWorkflow.yml"
-    while workflow_status(workflowFile) == 'queued':
-        print(workflow_status(workflowFile))
-        time.sleep(2)
+    # while workflow_status(workflowFile) == 'queued':
+    #     print(workflow_status(workflowFile))
+    #     time.sleep(2)
 
 print(workflow_status(workflowFile))
