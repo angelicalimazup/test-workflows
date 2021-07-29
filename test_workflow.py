f8903728feb5f896e200e931e0cb79c1dbf6c52e
Angelica Lima <65919486+angelicalimazup@users.noreply.github.com>
2021-07-29 14:41:53 +0100

    test

--- test_workflow.py
+++ test_workflow.py
@@ -6,3 +6,7 @@ class TestClass(TestCase):
     def test_fail_workflow(self):
         workflow_file = "failWorkflow.yml"
         assert "failure" == workflow_status(workflow_file)
+
+    def test_success_workflow(self):
+        workflow_file = "failWorkflow.yml"
+        assert "failure" == workflow_status(workflow_file)
